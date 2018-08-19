e106cb9c8a0a9e63b9ba1e58c378ec5204baf517
vinay raj <vinayraj33@live.com>
2018-08-19 14:29:21 +0530

    code done

---
+++ problem_statement_2.py
@@ -0,0 +1,11 @@
+
+# coding: utf-8
+
+# In[3]:
+
+
+print("""WE, THE PEOPLE OF INDIA,
+having solemnly resolved to constitute India into a SOVEREIGN, !
+SOCIALIST, SECULAR, DEMOCRATIC REPUBLIC
+and to secure to all its citizens:""")
+
